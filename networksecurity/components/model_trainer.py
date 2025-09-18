d2f392b477eb560cff199b9d080179e93514f67a
Vikash Kumar <vikash912kr@gmail.com>
2025-09-18 17:48:27 +0530

    model evaluation and mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -23,6 +23,7 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
+import mlflow
 
 class ModelTrainer:
     def __init__(self,model_trainer_config: ModelTrainerConfig,data_transformation_artifact: DataTransformationArtifact):
@@ -32,6 +33,17 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+    
+    def track_mlflow(self,best_model,classificationmetric):
+            with mlflow.start_run():
+                f1_score=classificationmetric.f1_score
+                precision_score=classificationmetric.precision_score
+                recall_score=classificationmetric.recall_score
+
+                mlflow.log_metric("f1_score",f1_score)
+                mlflow.log_metric("precision_score",precision_score)
+                mlflow.log_metric("recall_score",recall_score)
+                mlflow.sklearn.log_model(best_model,"model")
         
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
@@ -84,14 +96,14 @@ class ModelTrainer:
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
-        ## Track the experiments with mlflow
-        # self.track_mlflow(best_model,classification_train_metric)
-
+        ## Track the experiments with mlflow (mlflow is very useful understand the entire lifecycle of model)
+        self.track_mlflow(best_model,classification_train_metric)
+        
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
-
-        # self.track_mlflow(best_model,classification_test_metric)
+        # track test metrics for mlflow
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
